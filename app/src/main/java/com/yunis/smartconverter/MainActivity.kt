955686241503f2514a47765647af4fa6afa6d9e6
yuniskhamis <yuniskhamis3@gmail.com>
2021-07-26 17:11:40 -0400

    Added more ways to handle user input

--- app/src/main/java/com/yunis/smartconverter/MainActivity.kt
+++ app/src/main/java/com/yunis/smartconverter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.yunis.smartconverter
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,7 +13,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var input: EditText
     private lateinit var resetButton: Button
     private lateinit var convertButton: Button
+    private lateinit var toast1: Toast
+    private lateinit var toast2: Toast
+    private lateinit var toast3: Toast
 
+
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         resultText = findViewById(R.id.textView2)
         convertButton = findViewById(R.id.button)
         resetButton = findViewById(R.id.button2)
+        toast1 = Toast.makeText(this, "Enter a number in the input field, you entered a string.", Toast.LENGTH_SHORT)
+        toast2 = Toast.makeText(this, "Enter a number in the input field.", Toast.LENGTH_SHORT)
+        toast3 = Toast.makeText(this, "Input field already empty.", Toast.LENGTH_SHORT)
 
 
         val conversion = resources.getStringArray(R.array.Conversions)
@@ -37,80 +46,44 @@ class MainActivity : AppCompatActivity() {
                 override fun onItemSelected(parent: AdapterView<*>,
                                             view: View, position: Int, id: Long) {
 
-
                     when (parent.selectedItem) {
                         parent.getItemAtPosition(0) -> {
 
                             convertButton.setOnClickListener {
-
-
                                 convertFahrenheitToCelsius()
-
                             }
-                            resetButton.setOnClickListener {
-                                input.setText("")
-                                resultText.text = ""
-                            }
-
-
+                            reset()
                         }
                         parent.getItemAtPosition(1) -> {
 
                             convertButton.setOnClickListener {
-
                                 convertCelsiusToFahrenheit()
-
                             }
-                            resetButton.setOnClickListener {
-                                input.setText("")
-                                resultText.text = ""
-                            }
-
+                            reset()
                         }
                         parent.getItemAtPosition(2) -> {
                             convertButton.setOnClickListener {
-
                                 convertUsDollarsToEuros()
-
                             }
-                            resetButton.setOnClickListener {
-                                input.setText("")
-                                resultText.text = ""
-                            }
+                            reset()
                         }
                         parent.getItemAtPosition(3) -> {
                             convertButton.setOnClickListener {
-
                                 convertEurosToUsDollars()
-
                             }
-                            resetButton.setOnClickListener {
-                                input.setText("")
-                                resultText.text = ""
-                            }
+                            reset()
                         }
                         parent.getItemAtPosition(4) -> {
                             convertButton.setOnClickListener {
-
                                 convertPoundsToKilograms()
-
                             }
-                            resetButton.setOnClickListener {
-                                input.setText("")
-                                resultText.text = ""
-                            }
+                            reset()
                         }
                         parent.getItemAtPosition(5) -> {
                             convertButton.setOnClickListener {
-
-
                                 convertKilogramsToPounds()
-
                             }
-                            resetButton.setOnClickListener {
-                                input.setText("")
-                                resultText.text = ""
-                            }
+                            reset()
                         }
                     }
 
@@ -126,72 +99,98 @@ class MainActivity : AppCompatActivity() {
 
     fun convertFahrenheitToCelsius() {
         try {
-            val celsiusResult: Float = (input.text.toString().toFloat() - 32) * 5 / 9
-            resultText.text = celsiusResult.toString()
+            if (input.text.toString().isEmpty()) {
+                toast2.show()
+            } else {
+                val celsiusResult: Float = (input.text.toString().toFloat() - 32) * 5 / 9
+
+                resultText.text = celsiusResult.toString()
+            }
         } catch (NumberFormatException: Exception) {
-            val toast = Toast.makeText(this, "Enter an Integer date type, you entered a string", Toast.LENGTH_SHORT)
-            toast.show()
+            toast1.show()
 
         }
 
     }
 
     fun convertCelsiusToFahrenheit() {
-        // if (input.text != null) {
         try {
-            val fahrenheitResult: Float = (input.text.toString().toFloat() * 9 / 5) + 32
-            resultText.text = fahrenheitResult.toString()
+            if (input.text.toString().isEmpty()) {
+                toast2.show()
+            } else {
+                val fahrenheitResult: Float = (input.text.toString().toFloat() * 9 / 5) + 32
+                resultText.text = fahrenheitResult.toString()
+            }
         } catch (NumberFormatException: Exception) {
-            val toast = Toast.makeText(this, "Enter an Integer date type, you entered a string", Toast.LENGTH_SHORT)
-            toast.show()
+            toast1.show()
 
         }
-        //}
+
     }
 
     fun convertPoundsToKilograms() {
         try {
-            val eurosResult: Double = (input.text.toString().toDouble() / 2.2046)
-            resultText.text = eurosResult.toString()
+            if (input.text.toString().isEmpty()) {
+                toast2.show()
+            } else {
+                val eurosResult: Double = (input.text.toString().toDouble() / 2.2046)
+                resultText.text = eurosResult.toString()
+            }
         } catch (NumberFormatException: Exception) {
-            val toast = Toast.makeText(this, "Enter an Integer date type, you entered a string", Toast.LENGTH_SHORT)
-            toast.show()
-
+            toast1.show()
         }
-
     }
 
     fun convertKilogramsToPounds() {
         try {
-            val eurosResult: Double = (input.text.toString().toDouble() * 2.2046)
-            resultText.text = eurosResult.toString()
+            if (input.text.toString().isEmpty()) {
+                toast2.show()
+            } else {
+                val eurosResult: Double = (input.text.toString().toDouble() * 2.2046)
+                resultText.text = eurosResult.toString()
+            }
         } catch (NumberFormatException: Exception) {
-            val toast = Toast.makeText(this, "Enter an Integer date type, you entered a string", Toast.LENGTH_SHORT)
-            toast.show()
+            toast1.show()
 
         }
     }
 
     fun convertEurosToUsDollars() {
         try {
-            val eurosResult: Double = (input.text.toString().toDouble() / 0.823445)
-            resultText.text = eurosResult.toString()
+            if (input.text.toString().isEmpty()) {
+                toast2.show()
+            } else {
+                val eurosResult: Double = (input.text.toString().toDouble() / 0.823445)
+                resultText.text = eurosResult.toString()
+            }
 
         } catch (NumberFormatException: Exception) {
-            val toast = Toast.makeText(this, "Enter an Integer date type, you entered a string", Toast.LENGTH_SHORT)
-            toast.show()
+            toast1.show()
 
         }
     }
 
     fun convertUsDollarsToEuros() {
         try {
-            val eurosResult: Double = (input.text.toString().toDouble() * 0.823445)
-            resultText.text = eurosResult.toString()
+            if (input.text.toString().isEmpty()) {
+                toast2.show()
+            } else {
+                val eurosResult: Double = (input.text.toString().toDouble() * 0.823445)
+                resultText.text = eurosResult.toString()
+            }
         } catch (NumberFormatException: Exception) {
-            val toast = Toast.makeText(this, "Enter an Integer date type, you entered a string", Toast.LENGTH_SHORT)
-            toast.show()
+            toast1.show()
+        }
+    }
 
+    fun reset() {
+        resetButton.setOnClickListener {
+            if (input.text.toString().isEmpty()) {
+                toast3.show()
+            } else {
+                input.setText("")
+                resultText.text = ""
+            }
         }
     }
 }
